6f29c81f71ab578fccbc718e489d2d196d7f0992
Iván Carrillo <nawtovi@gmail.com>
2017-02-06 08:53:26 +0100

    Proyectos
    Añadiendo el proyecto de insertar en una base de datos

---
+++ InsertarUsuarioBBDD/InsertarUsuarioBBDD/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace InsertarUsuarioBBDD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult InsertarUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertarUsuario(string Nick, string Pass)
+        {
+            string sql = "INSERT INTO usuario(nick, pass) VALUES('"+Nick+"', '"+Pass+"')";
+            MySqlConnection connection = AbrirConexion();
+            MySqlCommand comando = new MySqlCommand(sql,connection);
+            comando.ExecuteNonQuery();
+            return View();
+        }
+
+        public static MySqlConnection AbrirConexion()
+        {
+            MySqlConnection connection = new MySqlConnection();
+                    connection.ConnectionString =
+                        "Server=localhost;Database=insertarBase;Uid=root;Pwd=''";
+                    connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ InsertarUsuarioBBDD/InsertarUsuarioBBDD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InsertarUsuarioBBDD.Global" Language="C#" %>
